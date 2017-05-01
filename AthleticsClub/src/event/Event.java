ded2706faebc2536c7b67c92e4e1c0b0f9b17d23
Alejandro Reyes Bautista <reyes.bautista.alejandro@gmail.com>
2017-05-01 14:27:41 +0100

    Training methods for obtaining training by differents parameters of searching changes. Same as Event methods.

--- AthleticsClub/src/event/Event.java
+++ AthleticsClub/src/event/Event.java
@@ -6,67 +6,129 @@ import java.util.List;
 
 /**
  *
- * @author Alejandro Reyes
+ * @author Alejandro Reyes (AlejandroReBa)
  */
 
 public class Event {
+    //invented
+    enum typeEnum {Football, Basketball, Futsal, Hockey, Voleyball, Handball,
+    Athletism, Cycling, Swimming, Badminton, Tennis, Padel, KickBoxing, Boxing};
+    enum genderEnum {Men, Women, Mixed};
+    enum ageGroupRelatedEnum {U13, U15, U17, U20, Senior, Masters};
+    enum transportEnum {Car, Bus, Minibus, Train};
+    
+    private static int incrementalId = 1;
+    public static List<Event> eventsList = new ArrayList<>();
+    
     private int id;
-    private String type;
-    private String gender;
-    private String ageGroup;
-    private String transport;
+    private typeEnum type;
+    private genderEnum gender;
+    private ageGroupRelatedEnum ageGroup;
+    private transportEnum transport;
     private Date date;
-    public static List<Event> eventList = new ArrayList<>();
-    private int official;
+    //not initialized yet, does not exist negative ID
+    private int officialId = -1;
+
     
     public Event (String typeIn, String genderIn, String ageGroupIn,
             String transportIn, Date dateIn){
-        this.type = typeIn;
-        this.gender = genderIn;
-        this.ageGroup = ageGroupIn;
-        this.transport = transportIn;
+        this.type = typeEnum.valueOf(typeIn);
+        this.gender = genderEnum.valueOf(genderIn);
+        this.ageGroup = ageGroupRelatedEnum.valueOf(ageGroupIn);
+        this.transport = transportEnum.valueOf(transportIn);
         this.date = dateIn;
         
-        eventList.add(this);
+        this.id = incrementalId;
+        incrementalId++; 
+        eventsList.add(this);
+    }
+    
+        public Event (String typeIn, String genderIn, String ageGroupIn,
+            String transportIn, Date dateIn, int officialIdIn){
+        this.type = typeEnum.valueOf(typeIn);
+        this.gender = genderEnum.valueOf(genderIn);
+        this.ageGroup = ageGroupRelatedEnum.valueOf(ageGroupIn);
+        this.transport = transportEnum.valueOf(transportIn);
+        this.date = dateIn;
+        this.officialId = officialIdIn; 
+        
+        this.id = incrementalId;
+        incrementalId++; 
+        eventsList.add(this);
     }
     
     public String getType (){
-        return null;
+        return this.type.toString();
     }
     
     public String getGender (){
-        return null;
+        return this.gender.toString();
     }
     
     public String getAgeGroup (){
-        return null;
+        return this.ageGroup.toString();
     }
     
     public String getTransport (){
-        return null;
+        return this.transport.toString();
     }
     
     public Date getDate (){
-        return null;
+        return this.date;
     }
     
-    public Event viewEventById(int id){
-        return null;
+    public int getOfficialId(){
+        return this.officialId;
     }
     
-    public List<Event> viewEventByType(String type){
-        return null;
+    public static Event viewEventById(int id){
+        Event resEvent = null;
+        for (Event currentEvent : eventsList){
+            if (currentEvent.id == id){
+                resEvent = currentEvent;
+                break;
+            }
+        }
+        return resEvent;
     }
     
-    public List<Event> viewEventByAgeGroup(String ageGroup){
-        return null;
+    public static List<Event> viewEventsByType(String type){
+        List<Event> resEvents = new ArrayList<>();
+        for (Event currentEvent : eventsList){
+            if (currentEvent.type.toString().equals(type)){
+                resEvents.add(currentEvent);
+            }
+        }
+        return resEvents;
     }
     
-    public List<Event> viewEventByGender(String gender){
-        return null;
+    public static List<Event> viewEventsByAgeGroup(String ageGroup){
+        List<Event> resEvents = new ArrayList<>();
+        for (Event currentEvent : eventsList){
+            if (currentEvent.ageGroup.toString().equals(ageGroup)){
+                resEvents.add(currentEvent);
+            }
+        }
+        return resEvents;
     }
     
-    public List<Event> viewEventByDate(Date date){
-        return null;
+    public static List<Event> viewEventsByGender(String gender){
+        List<Event> resEvents = new ArrayList<>();
+        for (Event currentEvent : eventsList){
+            if (currentEvent.gender.toString().equals(gender)){
+                resEvents.add(currentEvent);
+            }
+        }
+        return resEvents;
+    }
+    
+    public static List<Event> viewEventsByDate(Date date){
+        List<Event> resEvents = new ArrayList<>();
+        for (Event currentEvent : eventsList){
+            if (currentEvent.date.equals(date)){
+                resEvents.add(currentEvent);
+            }
+        }
+        return resEvents;
     }
 }
