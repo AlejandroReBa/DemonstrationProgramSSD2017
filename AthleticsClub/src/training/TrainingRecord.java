14cea5a9aa6cbfe3567da256c87b40e6b1873281
Alejandro Reyes Bautista <reyes.bautista.alejandro@gmail.com>
2017-05-01 18:05:48 +0100

    Added and modified some methods. Added a new constructor with distinct lists as parameters (list of events, list of athletes...)
    TrainingRecord has now an autoincremental ID.
    
    Modified Membership main constructor; added boolean parameter to avoid
    add the membershipt to the list before it has been completely initialized
    (at subclasses extending Membership)

---
+++ AthleticsClub/src/training/TrainingRecord.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package training;
+
+import java.util.List;
+
+/**
+ *
+ * @author Alejandro Reyes
+ */
+public class TrainingRecord { //another class because "Coaches hold separate records relating to their athletes, training routine..."
+    private static List<TrainingRecord> trainingRecordsList;
+    private static int incrementalId = 1;
+    
+    private int id;
+    private int athleteId;
+    private int coachId;
+    private int trainingId;
+    private String record;
+    
+    public TrainingRecord (int athleteIdIn, int coachIdIn, int trainingIdIn, String recordIn){
+        this.athleteId = athleteIdIn;
+        this.coachId = coachIdIn;
+        this.trainingId = trainingIdIn;
+        this.record = recordIn;
+        
+        this.id = incrementalId;
+        incrementalId++;
+        trainingRecordsList.add(this);
+        
+    }
+    
+    ///Should I to add getTrainingRecordbyId, or viewTrainingRecord by coach, athlete...?
+    public int getAthleteId(){
+        return this.athleteId;
+    }
+    
+    public int getCoachId(){
+        return this.coachId;
+    }
+    
+    public int getTrainingId(){
+        return this.trainingId;
+    }
+    
+    public String getRecord(){
+        return this.record;
+    }
+}
