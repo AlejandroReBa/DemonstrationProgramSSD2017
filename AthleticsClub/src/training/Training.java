d3a7c26929d705857df75c94f9774087b4e2f69a
Alejandro Reyes Bautista <reyes.bautista.alejandro@gmail.com>
2017-05-01 13:49:30 +0100

    Training methods for obtaining training by differents parameters of searching implemented.

--- AthleticsClub/src/training/Training.java
+++ AthleticsClub/src/training/Training.java
@@ -8,70 +8,120 @@ package training;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import membership.Coach;
 
 /**
  *
  * @author Alejandro Reyes
  */
 public class Training {
-    private int id;
-    private String type;
-    private int coach;
-    private String discipline;
-    private String ageGroup;
-    private Date date;
-    public static List<Training> trainingList = new ArrayList<>();
+    enum typeEnum {RoadDistance, XCountryDistance, Track, Field, Gym};
+    enum disciplineTrainingEnum {Hurdling, Sprinting, Jumping, Throwing};
+    enum ageGroupRelatedEnum {U13, U15, U17, U20, Senior, Masters};
     
-    public Training (String typeIn, int coachIn, String disciplineIn,
+    private static int incrementalId = 1;
+    public static List<Training> trainingsList = new ArrayList<>();
+    
+    private int id;
+    private typeEnum type;
+    private int coachId;
+    private disciplineTrainingEnum discipline;
+    private ageGroupRelatedEnum ageGroup;
+    private Date date;
+    
+    
+    public Training (String typeIn, int coachIdIn, String disciplineIn,
             String ageGroupIn, Date dateIn){
-        this.type = typeIn;
-        this.coach = coachIn;
-        this.discipline = disciplineIn;
-        this.ageGroup = ageGroupIn;
+        this.type = typeEnum.valueOf(typeIn);
+        this.coachId = coachIdIn;
+        this.discipline = disciplineTrainingEnum.valueOf(disciplineIn);
+        this.ageGroup = ageGroupRelatedEnum.valueOf(ageGroupIn);
         this.date = dateIn;
         
-        trainingList.add(this);
+        this.id = incrementalId;
+        incrementalId++;
+        
+        trainingsList.add(this);
+    }
+    
+    public int getId(){
+        return this.id;
     }
     
     public String getType(){
-        return this.type;
+        return this.type.toString();
     }
     
     public int getCoach(){
-        return this.coach;
+        return this.coachId;
     }
     
     public String getDiscipline(){
-        return this.discipline;
+        return this.discipline.toString();
     }
     
     public String getAgeGroup(){
-        return this.ageGroup;
+        return this.ageGroup.toString();
     }
     
     //TO DO
-    public Training viewTrainingById(int id){
-        return null;
+    public static Training viewTrainingById(int id){
+        Training resTraining = null;
+        for (Training currentTraining : trainingsList){
+            if (currentTraining.id == id){
+                resTraining = currentTraining;
+                break;
+            }
+        }
+        return resTraining;
     }
     
     //TO DO
-    public Training viewTrainingByType(String type){
-        return null;
+    public static Training viewTrainingByType(String type){
+        Training resTraining = null;
+        for (Training currentTraining : trainingsList){
+            if (currentTraining.type.toString().equals(type)){
+                resTraining = currentTraining;
+                break;
+            }
+        }
+        return resTraining;
     }
     
     //TO DO
-    public Training viewTrainingByDiscipline(String discipline){
-        return null;
+    public static Training viewTrainingByDiscipline(String discipline){
+        Training resTraining = null;
+        for (Training currentTraining : trainingsList){
+            if (currentTraining.discipline.toString().equals(discipline)){
+                resTraining = currentTraining;
+                break;
+            }
+        }
+        return resTraining;
     }
     
     //TO DO
-    public Training viewTrainingByAgeGroup(String ageGroup){
-        return null;
+    public static Training viewTrainingByAgeGroup(String ageGroup){
+        Training resTraining = null;
+        for (Training currentTraining : trainingsList){
+            if (currentTraining.ageGroup.toString().equals(ageGroup)){
+                resTraining = currentTraining;
+                break;
+            }
+        }
+        return resTraining;
     }
     
     //TO DO
-    public Training viewTrainingByDate(Date date){
-        return null;
+    public static Training viewTrainingByDate(Date date){
+        Training resTraining = null;
+        for (Training currentTraining : trainingsList){
+            if (currentTraining.date.equals(date)){ //perhaps is more complex..
+                resTraining = currentTraining;
+                break;
+            }
+        }
+        return resTraining;
     }
     
 }
